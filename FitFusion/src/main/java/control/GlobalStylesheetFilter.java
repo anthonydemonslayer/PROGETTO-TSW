a4a2460e1e229daca1a355fafcdea06ed03e95b0
immagiov4 <g.brancaccio9@studenti.unisa.it>
2024-07-05 21:45:23 +0200

    Iniziato css e navbar

---
+++ FitFusion/src/main/java/control/GlobalStylesheetFilter.java
@@ -0,0 +1,30 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebFilter(description = "Applica lo stile css di base a tutte le pagine del sito web", urlPatterns = { "/*" })
+public class GlobalStylesheetFilter extends HttpFilter implements Filter {
+       
+	@Override
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException{
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String contextPath = request.getContextPath();
+        String cssUrl = contextPath + "/css/global-style.css";
+        
+        httpResponse.setHeader("Global-CSS", "<link rel='stylesheet' type='text/css' href='"+cssUrl+"'>");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void destroy() {}
+
+}
--- FitFusion/src/main/webapp/index.jsp
+++ FitFusion/src/main/webapp/index.jsp
@@ -1,12 +1,25 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="ISO-8859-1"%>
+
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
-
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Home</title>
+		<%= response.getHeader("Global-CSS") %>
+	</head>
+	<body>	
+		<nav class="navbar">
+        <div class="titolo-piccolo">FIT FUSION</div>
+        <div class="navbar-menu">
+            <a href="#">HOME</a>
+            <a href="#">CONTATTACI</a>
+            <a href="#">LOGIN / SIGNUP</a>
+        </div>
+        <div class="navbar-buttons">
+            <button style = "background-image: url( 'images/carrello.svg' );" class="icon"></button>
+            <button style = "background-image: url( 'images/account.svg' );" class="icon"></button>
+        </div>
+    </nav>
+    
+	</body>
 </html>
